b0ed289b39cec61e3d3f9a01799796a77c524fba
TOMATONO-WIN10\tomat <tomatono@gmail.com>
2018-08-12 03:19:23 +0900

    コメント削除

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,18 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { SubC01Component } from './sub-c01/sub-c01.component';
 import { ProgressDemoComponent } from './progress-demo/progress-demo.component';
 import { RxjsDemoComponent } from './rxjs-demo/rxjs-demo.component';
-// import { TabsDemoComponent } from './tabs-demo/tabs-demo.component';
-// import { Tab01Component } from './tab01/tab01.component';
-// import { Tab02Component } from './tab02/tab02.component';
 
 const routes: Routes = [
     { path: 'sub-c01', component: SubC01Component },
     { path: 'progress-demo', component: ProgressDemoComponent },
     { path: 'rxjs-demo', component: RxjsDemoComponent },
     { path: 'tabs-demo', loadChildren: './tabs-demo/tabs.module#TabsModule' },
-    // { path: 'tabs-demo', component: TabsDemoComponent },
-    // { path: 'tabs-demo/tab01', component: Tab01Component },
-    // { path: 'tabs-demo/tab02', component: Tab02Component },
 ];
 
 @NgModule({
--- src/app/tabs-demo/tabs.module.ts
+++ src/app/tabs-demo/tabs.module.ts
@@ -1,12 +1,5 @@
-// import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-// import { FormsModule } from '@angular/forms';
-// import { HttpClientModule } from '@angular/common/http';
-// import { MatInputModule, MatButtonModule, MatTabGroup, MatTabsModule } from '@angular/material';
 import { MatTabsModule } from '@angular/material';
-
-// import { RouterModule, Routes } from '@angular/router';
 
 import { Tab01Component } from './tab01/tab01.component';
 import { Tab02Component } from './tab02/tab02.component';
@@ -18,9 +11,6 @@ import { Tab03Component } from './tab03/tab03.component';
 @NgModule({
     imports: [
         CommonModule,
-        // BrowserModule,
-        // FormsModule,
-        // HttpClientModule,
         MatTabsModule,
         TabsRoutingModule,
     ],
@@ -31,6 +21,5 @@ import { Tab03Component } from './tab03/tab03.component';
         Tab03Component,
     ],
     providers: [],
-    // bootstrap: [AppComponent]
 })
 export class TabsModule { }
